296b71e387b89657dab214714698180f30e6ddef
Sven Nielsen <post@svennielsen.de>
2025-01-13 06:37:24 +0100

    update .tosc

--- scripts/decompress.sh
+++ scripts/decompress.sh
@@ -14,5 +14,6 @@ t="xml_export/"
 
 for f in $(find . -maxdepth 1 -type f -name '*.tosc'); do
   echo "Decompressing $f to $t .. also formats the .xml a bit better to allow for better showing a git diff on it"
-  pigz -c -d < "$f" | sed -r 's#(<[a-z]+>)<#\1\n<#g' - > "$t$(basename $f .tosc).xml"
+  # pigz -c -d < "$f" | sed -r 's#(<[a-z]+>)<#\1\n<#g' - > "$t$(basename $f .tosc).xml"
+  pigz -c -d < "$f" > "$t$(basename $f .tosc).xml"
 done
