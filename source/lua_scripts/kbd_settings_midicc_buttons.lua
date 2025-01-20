95ad081668131c72cc8b6fd965b531c636d32594
Sven Nielsen <post@svennielsen.de>
2025-01-20 04:51:44 +0100

    chore: make kbd cc sense prettier

--- source/lua_scripts/kbd_settings_midicc_buttons.lua
+++ source/lua_scripts/kbd_settings_midicc_buttons.lua
@@ -1,6 +1,46 @@
 ---@diagnostic disable: lowercase-global, undefined-global
 local delay = 300 -- the maximum elapsed time between taps
 local last = 0
+local blinkDelay = 800
+local blinkLast = 0
+local sensing = false
+local enabled = false
+
+function update()
+  if sensing then
+    local now = getMillis()
+    if(now - blinkLast > blinkDelay) then
+      blinkLast = now
+      self.values.x = 1
+    end
+  end
+end
+
+function enableSensing()
+  sensing = true
+  self.properties.outline = true
+  self.parent.children.midi.messages.MIDI[1].enabled = false
+  root:notify('keySense', self.parent.children.midi)
+end
+
+function disableSensing()
+  sensing = false
+  self.properties.outline = false
+  self.parent.children.midi.messages.MIDI[1].enabled = true
+  root:notify('keyStop')
+end
+
+function enable()
+  self.parent.children.midi.messages.MIDI[1].enabled = true
+  self.parent.parent.parent.parent.children.keys:notify('settings')
+end
+
+function disable()
+  self.parent.children.midi.messages.MIDI[1].enabled = false
+  self.parent.children.label.values.text = ''
+  self.parent.tag = 0
+  self.parent.children.midi.values.x = 0
+end
 
 function onValueChanged(k)
   if k == 'touch' then
@@ -8,26 +48,13 @@ function onValueChanged(k)
       local now = getMillis()
       if(now - last < delay) then
         last = 0
-        self.values.x = 0
-        self.properties.outline = false
-        self.parent.children.label.values.text = ''
-        self.parent.tag = 0
-        self.parent.children.midi.values.x = 0
+        disableSensing()
+        disable()
       else
         last = now
+        if enabled then disable() else enable() end
+        if not sensing then enableSensing() else disableSensing() end
       end
-    end
-  elseif k == 'x' then
-    if self.values.x == 1 then
-      self.parent.children.midi.messages.MIDI[1].enabled = false
-      self.properties.outline = true
-      root:notify('keySense', self.parent.children.midi)
-      self.values.x = 0.1
-    elseif self.values.x == 0 then
-      self.properties.outline = false
-      self.parent.children.midi.messages.MIDI[1].enabled = true
-      root:notify('keyStop')
-      self.parent.parent.parent.parent.children.keys:notify('settings')
     end
   end
 end
