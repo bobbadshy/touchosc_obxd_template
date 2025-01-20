79870924b7a00a82c4dcc64eec182823d48c2e3f
Sven Nielsen <post@svennielsen.de>
2025-01-20 04:51:25 +0100

    fix: midi channel button

--- source/lua_scripts/update_midi_channel.lua
+++ source/lua_scripts/update_midi_channel.lua
@@ -2,55 +2,46 @@
 local obxd = root:findByName('obxdCtrls', true)
 local kbd = self.parent.parent.parent
 local allControls = {}
-local channel = 1
 
 
 function controlEligible(c)
-  -- Check if MIDI msg attached
   if #c.messages.MIDI > 0 then return true end
   return false
 end
 
 function getAllControls(c)
-  if c == nil then
-    print('ERROR')
-    return
-  end
-  for i = 1, #c.children do
-    getAllControls(c.children[i])
-  end
+  if c == nil then return end
+  for i = 1, #c.children do getAllControls(c.children[i]) end
   if controlEligible(c) then table.insert(allControls, c) end
 end
 
 function updateChannelTags(c)
   getAllControls(c)
-  for k,_ in pairs(allControls) do
-    c.tag = channel
+  for k, cc in pairs(allControls) do
+    cc.tag = tonumber(self.values.text) - 1
   end
 end
 
 function init()
-    updateChannelTags(obxd)
-    updateChannelTags(kbd)
-end    
+  updateChannelTags(obxd)
+  updateChannelTags(kbd)
+end
 
 function onReceiveNotify(c)
   if c == 'up' then
-    channel = math.min(15, channel+1)
+    self.values.text = math.min(16, tonumber(self.values.text) + 1)
   elseif c == 'down' then
-    channel = math.max(0, channel-1)
+    self.values.text = math.max(1, tonumber(self.values.text) - 1)
   end
-  self.values.text = channel+1
 end
 
 function onValueChanged(k)
   allControls = {}
   if k == 'text' then
     i = tonumber(self.values.text)
-    if i ~= nil then
-      channel = math.min(15, math.max(0, i-1))
-    end
-    self.values.text = channel + 1
+    if i == nil then i = 1 end
+    self.values.text = math.min(16, math.max(1, i))
     updateChannelTags(obxd)
+    updateChannelTags(kbd)
   end
 end
