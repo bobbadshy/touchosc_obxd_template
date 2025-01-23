8cdbee90f74c8d2c5ba521bb3d166150c2ecba6a
Sven Nielsen <post@svennielsen.de>
2025-01-23 07:26:11 +0100

    fix build scripts

--- scripts/plain.sh
+++ scripts/plain.sh
@@ -8,11 +8,12 @@
 
 echo -e "\n == Removing eye-candy from $XML_BUILD_PLAIN ==\n"
 
+mv "$XML_BUILD_PLAIN" "$XML_BUILD_PLAIN.bak"
 #####
 # All eye-candy backgrounds with extra design element were named "backdrop".
 # Here, we manually delete those nodes from the xml file before compressing.
 #
-xmlstarlet ed -d '//children/node[properties/property/value[contains(text(), "backdrop")]]' < "$XML_SOURCE_PRETTY" > "$XML_BUILD_PLAIN"
+xmlstarlet ed -d '//children/node[properties/property/value[contains(text(), "backdrop")]]' <  "$XML_BUILD_PLAIN.bak" > "$XML_BUILD_PLAIN"
 
 echo -e "Compressing $XML_BUILD_PLAIN >> $TOSC_BUILD_PLAIN"
 pigz -c -z < "$XML_BUILD_PLAIN" > "$TOSC_BUILD_PLAIN"
